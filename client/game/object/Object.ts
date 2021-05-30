82a9d4ec6011277e6f78ec92ce2304934d8cdaa6
jamieboy1337 <jamieboy1337@gmail.com>
2021-05-30 16:51:42 -0700

    first pass on Object

---
+++ client/game/object/Object.ts
@@ -0,0 +1,81 @@
+/**
+ * Objects are the simplest unit in-engine.
+ * A single object contains a unique ID (on construction), as well as some simple functions
+ * which are called throughout the object's lifecycle.
+ */
+
+import { IDGenerator } from "../../../util/IDGenerator";
+import { ObjectType } from "./ObjectType";
+
+const gen = new IDGenerator();
+
+export abstract class Object {
+  private id: number;
+  private created: boolean;
+  readonly type: ObjectType;
+
+  constructor() {
+    this.id = gen.getNewID();
+    this.created = false;
+    this.type = ObjectType.OBJECT;
+  }
+
+  setId(id: number) {
+    this.id = id;
+    gen.registerNewID(id);
+  }
+
+  getId(id: number) {
+    return this.id;
+  }
+
+  protected updateFunc() {
+    if (!this.created) {
+      this.create();
+      this.created = true;
+    }
+
+    this.update();
+
+    for (let child of this.getChildren()) {
+      child.updateFunc();
+    }
+  }
+
+  /**
+   * Returns a list of all children of this Object.
+   */
+  abstract getChildren() : Array<Object>;
+
+  /**
+   * Returns the child of this Object with the specified ID, or null if the child does not exist.
+   */
+  abstract getChild(id: number) : Object;
+
+  /**
+   * Create routine implemented by clients.
+   * Most calls pertaining to other components should probably go here, rather than the ctor.
+   * Called only once, before this object is updated for the first time.
+   */
+  protected create() {
+    // nop
+  }
+
+  /**
+   * Update routine implemented by clients.
+   * Called once per frame.
+   */
+  protected update() {
+    // nop
+  }
+
+  /**
+   * Destroy routine implemented by clients.
+   * There is no notion of a destructor here -- however, when the associated "scene" is destroyed,
+   * this method will be called exactly once.
+   */
+  protected destroy() {
+
+  }
+
+}
