a65a8f0df23d4e5d9b2db40586ee930f58e59c4b
jamieboy1337 <jamieboy1337@gmail.com>
2021-07-04 18:49:29 -0700

    swap test file to js

--- test/browser/modeltest.ts
+++ test/browser/modeltest.ts
@@ -2,11 +2,16 @@ import { expect } from "chai";
 import { FileLoader } from "../../client/ts/game/engine/loaders/FileLoader";
 import { GLTFLoaderImpl } from "../../client/ts/game/engine/loaders/internal/GLTFLoaderImpl";
 
+const canvas = document.getElementById("test-canvas") as HTMLCanvasElement;
+const gl = canvas.getContext("webgl");
+
 describe("GLTFLoader", function() {
-  it("Should read a plain scene", function() {
+  it("Should read a plain scene", async function() {
     let temp = new FileLoader();
-    let loader = new GLTFLoaderImpl(temp);
-    let res = loader.loadGLTFModel("../data/test.glb");
+    let loader = new GLTFLoaderImpl(temp, gl);
+    let res = await loader.loadGLTFModel("../data/test.glb");
     expect(res).is.not.null;
+    
+    console.log(res);
   })
 })
