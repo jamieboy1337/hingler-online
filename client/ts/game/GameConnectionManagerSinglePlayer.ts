8b8f6de7c692dc7f682d8ca6ef044f92db405f1f
jamieboy1337 <jamieboy1337@gmail.com>
2021-08-23 14:31:55 -0700

    added logic for collisions with bombs

--- client/ts/game/GameConnectionManagerSinglePlayer.ts
+++ client/ts/game/GameConnectionManagerSinglePlayer.ts
@@ -1,4 +1,4 @@
-import { vec3 } from "gl-matrix";
+import { vec2, vec3 } from "gl-matrix";
 import { GameContext } from "../engine/GameContext";
 import { GameObject } from "../engine/object/game/GameObject";
 import { GameConnectionManager } from "./GameConnectionManager";
@@ -9,6 +9,8 @@ import { TileID } from "./tile/TileID";
 import { LayerInstance } from "./tile/LayerInstance";
 
 const PLAYER_MOTION_STATES = [PlayerInputState.MOVE_LEFT, PlayerInputState.MOVE_RIGHT, PlayerInputState.MOVE_UP, PlayerInputState.MOVE_DOWN, PlayerInputState.IDLE];
+
+const MAX_BOMB_COUNT = 8;
 // implement as game object so that we can receive update from root object
 // alternatively: we give it to some manager component which promises to update it
 // the manager component can handle dialogue, etc.
@@ -19,12 +21,14 @@ export class GameConnectionManagerSinglePlayer extends GameObject implements Gam
   private playermotion: PlayerInputState;
   private layer: Map<number, LayerInstance>;
   private currentLayerInstanceID: number;
+  private bombCount: number;
   constructor(ctx: GameContext) {
     super(ctx);
     this.state = new SinglePlayerMapState(11);
     this.playerpos = [0, 0];
     this.layer = new Map();
     this.currentLayerInstanceID = 0;
+    this.bombCount = 0;
   }
 
   getMapState() {
@@ -51,6 +55,16 @@ export class GameConnectionManagerSinglePlayer extends GameObject implements Gam
 
   update() {
     let delta = this.getContext().getDelta();
+    // with bombs they have just placed down.
+    let collisionIgnoreList : Array<vec2> = [];
+    for (let inst of this.layer.values()) {
+      let pos = inst.position;
+      let delta = [Math.abs(pos[0] - this.playerpos[0]), Math.abs(pos[1] - this.playerpos[1])];
+      // eps factor -- in case float rounding becomes an issue, this should be negligible in gameplay however
+      if (delta[0] < 0.999 && delta[1] < 0.999) {
+        collisionIgnoreList.push([pos[0], pos[1]]);
+      }
+    }
 
     // snag current motion state
     switch (this.playermotion) {
@@ -93,7 +107,44 @@ export class GameConnectionManagerSinglePlayer extends GameObject implements Gam
         this.playerpos[1] = Math.round(this.playerpos[1]);
       }
     }
+
     // continue moving in that direction
+    // now we want to check our layers in the respective direction?
+    for (let inst of this.layer.values()) {
+      if (inst.type !== TileID.BOMB) {
+        continue;
+      }
+
+      let pos = inst.position;
+      // note: we do this check for every layer, regardless of if we actually care about collisions with it
+      let check : boolean = (pos[0] === checkX[0] && pos[1] === checkX[1]);
+      check = check || (pos[0] === checkY[0] && pos[1] === checkY[1]);
+
+      // neither tile collision is a problem -- bail out
+      if (!check) {
+        continue;
+      }
+
+      // ensure the location isn't on our "ignore" list
+      for (let ignore of collisionIgnoreList) {
+        if (pos[0] === ignore[0] && pos[1] === ignore[1]) {
+          check = false;
+          break;
+        }
+      }
+
+      if (!check) {
+        continue;
+      }
+
+      if ((pos[0] === checkX[0] && pos[1] === checkX[1])) {
+        this.playerpos[0] = Math.round(this.playerpos[0]);
+      }
+      
+      if (pos[0] === checkY[0] && pos[1] === checkY[1]) {
+        this.playerpos[1] = Math.round(this.playerpos[1]);
+      }
+    }
 
     // we also want to check our layers and see if an impassable object has been placed on one of these tiles
     // if so: stop the player from moving there!
@@ -122,10 +173,19 @@ export class GameConnectionManagerSinglePlayer extends GameObject implements Gam
      */
   }
 
+  getBombCount() {
+    return this.bombCount;
+  }
+
+  getBombMax() {
+    return MAX_BOMB_COUNT;
+  }
+
   private handleBombPlace() {
-    // check the player's current location
-    // round off to the nearest block
-    // place a bomb there
+    // max out
+    if (this.bombCount >= MAX_BOMB_COUNT) {
+      return;
+    }
     let bombPos : vec3 = [Math.round(this.playerpos[0]), Math.round(this.playerpos[1]), 0];
     // ensure there's no other bombs in that location (this shouldn't be a problem)
     for (let id of this.layer.keys()) {
@@ -145,10 +205,7 @@ export class GameConnectionManagerSinglePlayer extends GameObject implements Gam
       position: bombPos
     };
 
-    // force the player away from the bomb, or let them move away from it?
-    // add a check: if a player is inside a bomb BEFORE we move them, ignore the collision
-    // after we move, they might be inside it, so we can't consider that.
-    console.log(id);
     this.layer.set(id, layer);
+    this.bombCount++;
   }
 }
