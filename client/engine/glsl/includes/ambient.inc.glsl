a2ec64b084a5c88fe5c5c1861c65866dec8c05f7
jamieboy1337 <jamieboy1337@gmail.com>
2021-10-30 21:51:25 -0700

    fucky wucky

--- client/engine/glsl/includes/ambient.inc.glsl
+++
@@ -1,8 +0,0 @@
-struct AmbientLight {
-  vec4 color;
-  float intensity;
-};
-
-vec4 getAmbientColor(AmbientLight a) {
-  return a.color * a.intensity;
-}
--- client/engine/glsl/shadownotexture/shadownotexture.frag
+++
@@ -1,7 +0,0 @@
-#version 100
-
-precision highp float;
-
-void main() {
-  gl_FragColor = vec4(1.0);
-}
